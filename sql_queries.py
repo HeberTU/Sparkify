611ad68fc33d5df9697cb6427cda4ef477d767e8
HTRUJILLO <htrujillo@lucta.com>
2022-02-11 08:45:52 +0100

    Create and drop statements added to sql_queries

--- sql_queries.py
+++ sql_queries.py
@@ -1,26 +1,67 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+CREATE TABLE songplays (
+    songplay_id varchar,
+    start_time timestamp,
+    user_id varchar,
+    level varchar,
+    song_id int,
+    artist_id varchar,
+    session_id int,
+    location varchar,
+    user_agent varchar
+);
 """)
 
 user_table_create = ("""
+CREATE TABLE users (
+    user_id varchar,
+    first_name varchar,
+    last_name varchar,
+    gender varchar,
+    level varchar
+);
 """)
 
 song_table_create = ("""
+CREATE TABLE songs (
+    song_id int,
+    title varchar,
+    artist_id varchar,
+    year int,
+    duration float
+);
 """)
 
 artist_table_create = ("""
+CREATE TABLE artists (
+    artist_id varchar,
+    name varchar,
+    location varchar,
+    latitude float,
+    longitud float
+);
 """)
 
 time_table_create = ("""
+CREATE TABLE time (
+    start_time timestamp,
+    hour int,
+    day int,
+    week varchar ,
+    month int,
+    year int,
+    weekday varchar
+);
 """)
 
 # INSERT RECORDS
