2c020e0a0c0f94fe11437f5ab0b0eb758d99e889
HTRUJILLO <htrujillo@lucta.com>
2022-02-18 19:04:14 +0100

    Not Null sanity check added.

--- etl.py
+++ etl.py
@@ -29,15 +29,15 @@ def process_song_file(cur: Cursor, filepath: str) -> None:
     # insert artist record
     artist_data = df[
         ['artist_id', 'artist_name', 'artist_location',
-         'artist_latitude', 'artist_longitude']].\
-        values.\
+         'artist_latitude', 'artist_longitude']]. \
+        values. \
         tolist()[0]
     cur.execute(artist_table_insert, artist_data)
 
     # insert song record
     song_data = df[
-        ['song_id', 'title', 'artist_id', 'year', 'duration']].\
-        values.\
+        ['song_id', 'title', 'artist_id', 'year', 'duration']]. \
+        values. \
         tolist()[0]
     cur.execute(song_table_insert, song_data)
 
@@ -58,7 +58,7 @@ def process_log_file(cur: Cursor, filepath: str) -> None:
 
     # convert timestamp column to datetime
     t = pd.to_datetime(df.ts, unit='ms')
-    
+
     # insert time data records
     time_data = {
         "start_time": t,
@@ -76,7 +76,7 @@ def process_log_file(cur: Cursor, filepath: str) -> None:
 
     # load user table
     user_df = df[
-        ['userId', 'firstName', 'lastName', 'gender', 'level']].\
+        ['userId', 'firstName', 'lastName', 'gender', 'level']]. \
         drop_duplicates()
 
     # insert user records
@@ -85,11 +85,11 @@ def process_log_file(cur: Cursor, filepath: str) -> None:
 
     # insert songplay records
     for index, row in df.iterrows():
-        
+
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
-        
+
         if results:
             songid, artistid = results
         else:
@@ -106,7 +106,8 @@ def process_log_file(cur: Cursor, filepath: str) -> None:
             row.location,
             row.userAgent
         )
-        cur.execute(songplay_table_insert, songplay_data)
+        if (not songid is None) and (not artistid is None):
+            cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(
@@ -124,8 +125,8 @@ def process_data(
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root,'*.json'))
-        for f in files :
+        files = glob.glob(os.path.join(root, '*.json'))
+        for f in files:
             all_files.append(os.path.abspath(f))
 
     # get total number of files found
@@ -141,7 +142,8 @@ def process_data(
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect(
+        "host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
