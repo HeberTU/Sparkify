884647afd308655dc7f9f0278e9c54b08044f9d2
HeberTU <heber.trujillo@glovoapp.com>
2022-06-18 14:59:08 +0200

    [NEW] Adding unittest for package versioning at new release.

---
+++ tests/unit/test_packaging_semantic_version.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""This module checks if the package version has been change for a new release.
+
+Created on: 18/6/22
+@author: Heber Trujillo <heber.trj.urt@gmail.com>
+Licence,
+"""
+from sparkifydb import __version__
+
+
+def test_version():
+    """Test package version for new release."""
+    assert __version__ == '0.1.0'
