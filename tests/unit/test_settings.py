cdd992e40ef862b193d9228cfef66008a7cdaa46
HeberTU <heber.trujillo@glovoapp.com>
2022-06-20 20:07:48 +0200

    [NEW] Adding markers definitions and unit test for setting management system.

--- tests/unit/test_settings.py
+++ tests/unit/test_settings.py
@@ -9,25 +9,30 @@ Licence,
 """
 import os
 from pathlib import Path
+import pytest
 from pydantic import BaseSettings
 from sparkifydb.core.settings import settings, Settings
 
 
+@pytest.mark.unit
 def test_setting_is_subclass_of_base_settings():
     """Test that Settings class inherits from BaseSettings."""
     assert issubclass(Settings, BaseSettings)
 
 
+@pytest.mark.unit
 def test_version_proyect_path():
     """Test the project path."""
     assert settings.PROJECT_PATH == Path(__file__).parents[2]
 
 
+@pytest.mark.unit
 def test_version_configurations_path():
     """Test the configuration path."""
     assert settings.CONFIG_PATH == settings.PROJECT_PATH / "config"
 
 
+@pytest.mark.unit
 def test_configurations_path_exists():
     """Test if configuration path exists."""
     assert os.path.exists(
@@ -35,6 +40,7 @@ def test_configurations_path_exists():
     )
 
 
+@pytest.mark.unit
 def test_db_credentials_have_valid_values():
     """Test if the databse credential were correctly loaded from .env file."""
     assert (
