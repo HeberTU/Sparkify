1720cc9c1f7a768976c4136ecffda6c9ccdaf26d
HeberTU <heber.trujillo@glovoapp.com>
2022-06-29 20:21:27 +0200

    [UPD] N-Layer architecture refactored.

---
+++ sparkifydb/db/repository.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Repository module.
+
+Created on: 29/6/22
+@author: Heber Trujillo <heber.trj.urt@gmail.com>
+Licence,
+"""
---
+++ sparkifydb/db/tables.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Relations module.
+
+Created on: 29/6/22
+@author: Heber Trujillo <heber.trj.urt@gmail.com>
+Licence,
+"""
